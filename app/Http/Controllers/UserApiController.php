bfb82d65b4c143e917af165587e28cc5a64aaa50
Programmer Munna <programmermunna@gmail.com>
2023-10-04 15:09:50 +0600

    api_crud_with_jwt_token_in_laravel

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -12,8 +12,8 @@ class UserApiController extends Controller
     //show single or multiple user
     public function ShowUser($id=null){
         if($id==''){
-            $users = User::get();
-            return response()->json(['users'=>$users],200);
+        $users = User::get();
+        return response()->json(['users'=>$users],200);
         }else{
             $users = User::find($id);
             return response()->json(['users'=>$users],200);
@@ -22,162 +22,227 @@ class UserApiController extends Controller
 
     //add single user
     public function AddUser(Request $request){
-        $users = $request->all();
-        $rules = [
-            'name'=>'required',
-            'email'=>'required|email|unique:users',
-            'password'=>'required',
-        ];
-        $custom_msg = [
-            'name.required'=>'Name is required',
-            'email.required'=>'Email is required',
-            'email.email'=>'Email must be valid',
-            'password.required'=>'Password is required',
-        ];
-        $validator = Validator::make($users,$rules,$custom_msg);
-        if($validator->fails()){
-            return response()->json($validator->errors(), 422);
-        }
-
-            User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => bcrypt($request->password),
-            ]);
-            $message = 'User Created Successfully';
-            return response()->json(['message'=>$message], 201);
+        $header = $request->header('Authorization');
+        if($header==''){
+            $message = 'Authorization is required';
+            return response()->json(['message'=>$message], 422);
+        }else{
+            if($header=='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJwcm9ncmFtbWVybXVubmFAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.Al6ar03o_DKlYhCndeVdMkJQ5L4JcSEsFux7NMbvwZY'){
+                $users = $request->all();
+                $rules = [
+                    'name'=>'required',
+                    'email'=>'required|email|unique:users',
+                    'password'=>'required',
+                ];
+                $custom_msg = [
+                    'name.required'=>'Name is required',
+                    'email.required'=>'Email is required',
+                    'email.email'=>'Email must be valid',
+                    'password.required'=>'Password is required',
+                ];
+                $validator = Validator::make($users,$rules,$custom_msg);
+                if($validator->fails()){
+                    return response()->json($validator->errors(), 422);
+                }
+        
+                    User::create([
+                        'name' => $request->name,
+                        'email' => $request->email,
+                        'password' => bcrypt($request->password),
+                    ]);
+                    $message = 'User Created Successfully';
+                    return response()->json(['message'=>$message], 201);
+            }else{
+                $message = 'Authorization dose not match';
+                return response()->json(['message'=>$message], 422); 
+            }
+        }        
     }
 
     //add multiple users by json
     public function AddMultipleUser(Request $request){
-        $users = $request->all();        
-        $rules = [
-            'users.*.name'=>'required',
-            'users.*.email'=>'required|email|unique:users',
-            'users.*.password'=>'required',
-        ];
-        $custom_msg = [
-            'users.*.name.required'=>'Name is required',
-            'users.*.email.required'=>'Email is required',
-            'users.*.email.email'=>'Email must be valid',
-            'users.*.password.required'=>'Password is required',
-        ];
-        $validator = Validator::make($users,$rules,$custom_msg);
-        if($validator->fails()){
-            return response()->json($validator->errors(), 422);
-        }
+        $header = $request->header('Authorization');
+        if($header==''){
+            $message = 'Authorization is required';
+            return response()->json(['message'=>$message], 422);
+        }else{
+            if($header=='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJwcm9ncmFtbWVybXVubmFAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.Al6ar03o_DKlYhCndeVdMkJQ5L4JcSEsFux7NMbvwZY'){
+                $users = $request->all();        
+                $rules = [
+                    'users.*.name'=>'required',
+                    'users.*.email'=>'required|email|unique:users',
+                    'users.*.password'=>'required',
+                ];
+                $custom_msg = [
+                    'users.*.name.required'=>'Name is required',
+                    'users.*.email.required'=>'Email is required',
+                    'users.*.email.email'=>'Email must be valid',
+                    'users.*.password.required'=>'Password is required',
+                ];
+                $validator = Validator::make($users,$rules,$custom_msg);
+                if($validator->fails()){
+                    return response()->json($validator->errors(), 422);
+                }
 
-        foreach($users['users'] as $user){
-            User::create([
-                'name' => $user['name'],
-                'email' => $user['email'],
-                'password' => bcrypt($user['password']),
-            ]);
-        }    
-            
-            $message = 'Multiple User Created Successfully';
-            return response()->json(['message'=>$message], 201);
+                foreach($users['users'] as $user){
+                    User::create([
+                        'name' => $user['name'],
+                        'email' => $user['email'],
+                        'password' => bcrypt($user['password']),
+                    ]);
+                }            
+                    $message = 'Multiple User Created Successfully';
+                    return response()->json(['message'=>$message], 201);
+            }else{
+                $message = 'Authorization dose not match';
+                return response()->json(['message'=>$message], 422); 
+            }
+        }        
     }
 
     //put api for update users data
     public function UpdateUserDetails(Request $request,$id){
-        $users = $request->all();
-        $rules = [
-            'name'=>'required',
-            'email'=>'required',
-            'password'=>'required',
-        ];
-        $custom_msg = [
-            'name.required'=>'Name is required',
-            'email.required'=>'Email is required',
-            'password.required'=>'Password is required',
-        ];
-        $validator = Validator::make($users,$rules,$custom_msg);
-        if($validator->fails()){
-            return response()->json($validator->errors(), 422);
-        }
-
-        $user = User::findOrFail($id);
-        if(!empty($request->password)){
-            $password = bcrypt($request->password);
+        $header = $request->header('Authorization');
+        if($header==''){
+            $message = 'Authorization is required';
+            return response()->json(['message'=>$message], 422);
         }else{
-            $password = $user->password;
-        }
-        
-            User::findOrFail($id)->update([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => $password,
-            ]);
+            if($header=='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJwcm9ncmFtbWVybXVubmFAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.Al6ar03o_DKlYhCndeVdMkJQ5L4JcSEsFux7NMbvwZY'){
+                $users = $request->all();
+                $rules = [
+                    'name'=>'required',
+                    'email'=>'required',
+                    'password'=>'required',
+                ];
+                $custom_msg = [
+                    'name.required'=>'Name is required',
+                    'email.required'=>'Email is required',
+                    'password.required'=>'Password is required',
+                ];
+                $validator = Validator::make($users,$rules,$custom_msg);
+                if($validator->fails()){
+                    return response()->json($validator->errors(), 422);
+                }
 
-            $message = 'Multiple User Updated Successfully';
-            return response()->json(['message'=>$message], 201);
+                $user = User::findOrFail($id);
+                if(!empty($request->password)){
+                    $password = bcrypt($request->password);
+                }else{
+                    $password = $user->password;
+                }
+                
+                    User::findOrFail($id)->update([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => $password,
+                    ]);
+
+                    $message = 'Multiple User Updated Successfully';
+                    return response()->json(['message'=>$message], 201);
+            }else{
+                $message = 'Authorization dose not match';
+                return response()->json(['message'=>$message], 422); 
+            }
+        }        
     }
 
     //patch api for update single users data
     public function UpdateSingleUser(Request $request,$id){
-        $users = $request->all();
-        $rules = [
-            'name'=>'required',
-            'email'=>'required',
-            'password'=>'required',
-        ];
-        $custom_msg = [
-            'name.required'=>'Name is required',
-            'email.required'=>'Email is required',
-            'password.required'=>'Password is required',
-        ];
-        $validator = Validator::make($users,$rules,$custom_msg);
-        if($validator->fails()){
-            return response()->json($validator->errors(), 422);
-        }
-
-        $user = User::findOrFail($id);
-        if(!empty($request->password)){
-            $password = bcrypt($request->password);
+        $header = $request->header('Authorization');
+        if($header==''){
+            $message = 'Authorization is required';
+            return response()->json(['message'=>$message], 422);
         }else{
-            $password = $user->password;
-        }
+            if($header=='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJwcm9ncmFtbWVybXVubmFAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.Al6ar03o_DKlYhCndeVdMkJQ5L4JcSEsFux7NMbvwZY'){
+                $users = $request->all();
+                $rules = [
+                    'name'=>'required',
+                    'email'=>'required',
+                    'password'=>'required',
+                ];
+                $custom_msg = [
+                    'name.required'=>'Name is required',
+                    'email.required'=>'Email is required',
+                    'password.required'=>'Password is required',
+                ];
+                $validator = Validator::make($users,$rules,$custom_msg);
+                if($validator->fails()){
+                    return response()->json($validator->errors(), 422);
+                }
         
-            User::findOrFail($id)->update([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => $password,
-            ]);
-
-            $message = 'Single User Updated Successfully';
-            return response()->json(['message'=>$message], 201);
+                $user = User::findOrFail($id);
+                if(!empty($request->password)){
+                    $password = bcrypt($request->password);
+                }else{
+                    $password = $user->password;
+                }
+                
+                    User::findOrFail($id)->update([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => $password,
+                    ]);
+        
+                    $message = 'Single User Updated Successfully';
+                    return response()->json(['message'=>$message], 201);
+            }else{
+                $message = 'Authorization dose not match';
+                return response()->json(['message'=>$message], 422); 
+            }
+        }
     }
 
     //delete api for delete single users data
     public function DeleteSingleUser($id=null){
         User::findOrFail($id)->delete();
         $message = 'Single User Deleted Successfully';
-        return response()->json(['message'=>$message], 201);
+        return response()->json(['message'=>$message], 201);        
     }
 
     //delete api for delete single users data with json
     public function JsonDeleteSingleUser(Request $request){
-        $users = $request->all();
-        User::where('id',$users['id'])->delete();
-        $message = 'Single User Deleted Successfully';
-        return response()->json(['message'=>$message], 201);
+        $header = $request->header('Authorization');
+        if($header==''){
+            $message = 'Authorization is required';
+            return response()->json(['message'=>$message], 422);
+        }else{
+            if($header=='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJwcm9ncmFtbWVybXVubmFAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.Al6ar03o_DKlYhCndeVdMkJQ5L4JcSEsFux7NMbvwZY'){
+                $users = $request->all();
+                User::where('id',$users['id'])->delete();
+                $message = 'Single User Deleted Successfully';
+                return response()->json(['message'=>$message], 201);
+            }else{
+                $message = 'Authorization dose not match';
+                return response()->json(['message'=>$message], 422); 
+            }
+        }        
     }
 
     //delete api for delete multiple users data
     public function DeleteMultipleUsers($ids){
-        $ids = explode(',',$ids);
-        User::whereIn('id',$ids)->delete();
-        $message = 'Multiple Users Deleted Successfully';
-        return response()->json(['message'=>$message], 201);
+            $ids = explode(',',$ids);
+            User::whereIn('id',$ids)->delete();
+            $message = 'Multiple Users Deleted Successfully';
+            return response()->json(['message'=>$message], 201);               
     }
 
     
     //delete api for delete multiple user data with json
     public function JsonDeleteMultipleUser(Request $request){
-        $users = $request->all();
-        User::whereIn('id',$users['ids'])->delete();
-        $message = 'Multiple Users Deleted Successfully';
-        return response()->json(['message'=>$message], 201);
+        $header = $request->header('Authorization');
+        if($header==''){
+            $message = 'Authorization is required';
+            return response()->json(['message'=>$message], 422);
+        }else{
+            if($header=='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJwcm9ncmFtbWVybXVubmFAZ21haWwuY29tIiwiaWF0IjoxNTE2MjM5MDIyfQ.Al6ar03o_DKlYhCndeVdMkJQ5L4JcSEsFux7NMbvwZY'){
+                $users = $request->all();
+                User::whereIn('id',$users['ids'])->delete();
+                $message = 'Multiple Users Deleted Successfully';
+                return response()->json(['message'=>$message], 201);
+            }else{
+                $message = 'Authorization dose not match';
+                return response()->json(['message'=>$message], 422); 
+            }
+        }
     }
 }
